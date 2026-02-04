bf506ae5b21a7a359d14077def5603cd922a584d
matiasagv <61482883+matiasagv@users.noreply.github.com>
2026-02-04 00:46:55 +0000

    a vers

--- brujilapp/src/app/pages/libro-sombras/libro-sombras.page.ts
+++ brujilapp/src/app/pages/libro-sombras/libro-sombras.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-libro-sombras',
   templateUrl: './libro-sombras.page.html',
   styleUrls: ['./libro-sombras.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LibroSombrasPage implements OnInit {
+  nuevoHechizo: string = '';
+  entradas: string[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.storage.create();
+    this.cargarEntradas();
   }
 
+  async guardarEntrada() {
+    if (this.nuevoHechizo) {
+      this.entradas.push(this.nuevoHechizo);
+      await this.storage.set('mi_libro_sombras', this.entradas);
+      this.nuevoHechizo = ''; // Limpiar el campo
+    }
+  }
+
+  async cargarEntradas() {
+    const guardado = await this.storage.get('mi_libro_sombras');
+    if (guardado) {
+      this.entradas = guardado;
+    }
+  }
 }
--- brujilapp/src/main.ts
+++ brujilapp/src/main.ts
@@ -1,14 +1,26 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core'; // <--- OJO AQUÍ: importProvidersFrom
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+// 1. Importar el módulo de Storage
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    
+    // 2. Aquí inicializas el Storage para toda la app
+    importProvidersFrom(IonicStorageModule.forRoot()) 
   ],
 });
