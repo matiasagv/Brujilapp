138d7e74797ef20e66b41e5da326ecda75716ce6
matiasagv <61482883+matiasagv@users.noreply.github.com>
2026-02-05 02:46:56 +0000

    aquitrabaje, la cague ajsfhg

--- brujilapp/src/app/pages/temas/temas.page.ts
+++ brujilapp/src/app/pages/temas/temas.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
 // Agregamos nuevos iconos para las nuevas tiradas: star, body, planet, calendar, walk
 import { close, albums, grid, heart, helpCircle, arrowUpCircle, arrowDownCircle, star, body, planet, calendar, walk } from 'ionicons/icons';
